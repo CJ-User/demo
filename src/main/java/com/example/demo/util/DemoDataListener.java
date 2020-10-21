1f618e8467e230aea31ead7784480d2d18d07ba2
chenjie <156335907@qq.com>
2020-10-21 11:21:07 +0800

    easy excel handle

--- src/main/java/com/example/demo/util/DemoDataListener.java
+++ src/main/java/com/example/demo/util/DemoDataListener.java
@@ -3,9 +3,11 @@ package com.example.demo.util;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class DemoDataListener extends AnalysisEventListener<DemoData> {
@@ -13,6 +15,8 @@ public class DemoDataListener extends AnalysisEventListener<DemoData> {
     private List correctList = new ArrayList<DemoData>();
     private List errorList = new ArrayList<DemoData>();
     private List existList = new ArrayList<DemoData>();
+    private List snList = new ArrayList<String>();
+    private int line = 1;
 
     @Override
     public void invoke(DemoData demoData, AnalysisContext analysisContext) {
@@ -23,7 +27,6 @@ public class DemoDataListener extends AnalysisEventListener<DemoData> {
             log.error("demoData == null");
             // todo throw "请确定文件是否为空"
         }
-        int line = 1;
         demoData.setLine(line);
 
         if (demoData.getName() == null) {
@@ -35,15 +38,32 @@ public class DemoDataListener extends AnalysisEventListener<DemoData> {
             correctList.add(demoData);
         }
 
+        if (demoData.getSn() == null) {
+            log.error("demoData.getName() == null");
+            // todo throw "请确'姓名'列是否标题是否正确"
+        } else if (demoData.getName().isEmpty()) {
+            errorList.add(demoData);
+        } else {
+            correctList.add(demoData);
+            snList.add(demoData.getSn().trim());
+        }
+
         line++;
     }
 
     @Override
     public void doAfterAllAnalysed(AnalysisContext analysisContext) {
         log.info("finish handle excel");
-        System.out.println(correctList.toArray());
-        System.out.println(errorList.toArray());
-//        connerList = connerBiz.findBySnList(snList, adminId);
+        correctList.forEach(System.out::println);
+        errorList.forEach(System.out::println);
+        snList.forEach(System.out::println);
+
+        // todo 查询已存在的数据
+//        List<Conner> connerList = null;
+//        if (!CollectionUtils.isEmpty(snList)) {
+//            connerList = connerBiz.findBySnList(snList, adminId);
+//        }
+//        List<Conner> list = connerList.stream().filter(p -> p.getSn().equals(sn)).collect(Collectors.toList());
 
 
     }
